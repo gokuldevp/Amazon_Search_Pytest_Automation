2c8f4ed34e3da386aa4947a243e2ad53e4584e7d
gokuldev <gokuldev.p123@gmail.com>
2025-02-01 10:52:04 +0530

    .

--- configs/configs.py
+++ configs/configs.py
@@ -1,8 +1,8 @@
 BASE_URL = "https://www.amazon.in/"
-SEARCH_ITEMS = ["mobile", "laptop", "headphones"]
+SEARCH_ITEMS = ["mobile"]#, "laptop", "headphones"]
 TIMEOUT = 15
 SCREEN_SIZES = {
     "desktop": (1920, 1080),
-    "tablet": (768, 1024),
-    "mobile": (375, 667)
+    # "tablet": (768, 1024),
+    # "mobile": (375, 667)
 }
